e8fec5ecc0923f23c1c6322a2d529ca2b81eea41
Diego-Capu <diegoospino487@gmail.com>
2026-03-09 21:30:57 -0500

    Please enter the commit message for your changes. Lines starting

---
+++ Riwi_code/calculadora.py
@@ -0,0 +1,24 @@
+print ("Calculadora de promedio")
+try:
+   
+        
+
+        ingles = float(input("Ingrese su nota de ingles:"))
+        socio = float(input("Ingrese su nota de socio: "))
+        desarrollo = float(input("Inhrese su nota de desarrollo: "))
+
+        nota1 = ingles * 0.20
+        nota2 = socio * 0.20
+        nota3 = desarrollo * 0.60
+
+        resultado = nota1 + nota2 + nota3 / 3
+
+        print(f"Tu promedio es de: {resultado}")
+
+        if resultado >=3.0:
+            print ("Aprobaste")
+        else:
+            print("Reprobaste")
+
+except ValueError as e:
+    print("Solo se permiten numeros")
